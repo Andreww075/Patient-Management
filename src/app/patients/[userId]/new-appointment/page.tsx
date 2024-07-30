ff7c75e32e170d75979b3ab3e76f92cdbf7e6650
Nicolás Gómez <Nvandres079@gmail.com>
2024-07-30 11:13:58 -0500

    AppointmentForm added

--- src/app/patients/[userId]/new-appointment/page.tsx
+++ src/app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import AppointmentForm from "@/components/forms/appointment-form";
+import { getPatient } from "@/lib/actions/patient.actions";
 
-const Appointment = () => {
+const Appointment = async ({ params: { userId } }: SearchParamProps) => {
+  const patient = await getPatient(userId);
+
   return (
-    <div>Appointment</div>
-  )
-}
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
 
-export default Appointment
+          <AppointmentForm
+            patientId={patient?.$id}
+            userId={userId}
+            type="create"
+          />
+
+          <p className="copyright mt-10 py-12">© 2024 CarePluse</p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1500}
+        width={1500}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom rounded-tl-3xl rounded-bl-3xl"
+      />
+    </div>
+  );
+};
+
+export default Appointment;
---
+++ src/app/patients/[userId]/new-appointment/success/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RequestSuccess = () => {
+  return (
+    <div>RequestSuccess</div>
+  )
+}
+
+export default RequestSuccess
